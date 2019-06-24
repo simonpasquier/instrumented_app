d66a87f9027dceeea4b806a0aa4e1c2ee1496091
Simon Pasquier <spasquie@redhat.com>
2019-06-24 11:26:19 +0200

    Fix container-push.sh

--- scripts/image-push.sh
+++ scripts/image-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 
-echo "$QUAY_PASSWORD" | docker login -u "$QUAY_USERNAME" --password-stdin
+echo "$QUAY_PASSWORD" | docker login -u "$QUAY_USERNAME" --password-stdin quay.io
 
 make push
